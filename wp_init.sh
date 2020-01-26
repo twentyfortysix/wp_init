deadc0f4b185fc02cf261830239d2fef0de34a75
twentyfortysix <2046@2046.cz>
2020-01-26 16:28:45 +0100

    Update wp_init.sh
    almond url fix

--- wp_init.sh
+++ wp_init.sh
@@ -174,7 +174,7 @@ wp plugin delete hello
 
 #download the Amond milk forst and activate
 cd wp-content/themes/
-wget https://github.com/twentyfortysix/almond-milk/archive/master.zip
+wget https://github.com/twentyfortysix/almond-milk.git
 unzip master.zip
 mv almond-milk-master/Almond-milk/ ./
 rm -R almond-milk-master/
